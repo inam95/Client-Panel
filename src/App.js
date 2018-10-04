d980ace1f1a56844a0a9da2632626bf5ad7e09b0
inam95 <mfihassan99@gmail.com>
2018-10-04 19:49:43 +0530

    Dashboard done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import "./App.css";
+import AppNavbar from "./components/layout/AppNavbar";
+import Dashboard from "./components/clients/Dashboard";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <Dashboard />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/clients/Clients.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Clients extends Component {
+  render() {
+    const clients = [
+      {
+        id: "45454545",
+        firstName: "Leo",
+        lastName: "Messi",
+        email: "leo@leo.com",
+        phone: "555-555-5555",
+        balance: "30"
+      },
+      {
+        id: "7878787",
+        firstName: "Neymar",
+        lastName: "Jr",
+        email: "ney@ney.com",
+        phone: "555-555-4545",
+        balance: "500"
+      }
+    ];
+
+    if (clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                <i className="fas fa-users" /> Clients
+              </h2>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <table className="table table-stripped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>{parseFloat(client.balance).toFixed(2)}</td>
+                  <td>
+                    <Link
+                      to={`/client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" /> Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>Loading...</h1>;
+    }
+  }
+}
+
+export default Clients;
